b52a20d8370b4e0483776264db3f68d8d3512a45
vsoni101 <vs201100@gmail.com>
2020-10-03 20:16:26 +0530

    Finish Navbar

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-   res.render("main")
+   res.render("index")
 })
 
 const port = process.env.PORT || 3000;
